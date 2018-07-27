017c13d8f2fc6be047a516dd04d00cca51383c5d
Nola <40751611+NolaWu@users.noreply.github.com>
2018-07-27 21:23:48 +0800

    Add files via upload

---
+++ install_docker.sh
@@ -0,0 +1,19 @@
+#!user/bin/bash
+
+#enviroment setting
+sudo apt-get install curl
+
+#install docker
+curl -fsSL https://get.docker.com/ | sh
+sudo usermod -aG docker yourname
+
+#start docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+#build choice one /dockerfile build your image
+#docker build -t <userName>/<tag> <dockerfile_path>
+ 
+#run docker/build container
+#docker run -t -i --name <container_name> iiiedu/step2 <command>
+
